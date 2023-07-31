adb6aba390e00b909d3099dadced5f75588d9705
seongu <tjsrn183@naver.com>
2023-07-31 19:15:00 +0900

    백엔드 설정2

---
+++ app.ts
@@ -0,0 +1,29 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const pageRouter = require("./routes/page");
+
+const app = express();
+app.set("port", process.env.PORT || 8001);
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
