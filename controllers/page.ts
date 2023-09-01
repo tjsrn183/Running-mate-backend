e7120c6647d46bb185435176701e3d88874b70c0
seongu <tjsrn183@naver.com>
2023-09-02 00:06:48 +0900

    글전체를 가져오는 라우터 작성

--- controllers/page.ts
+++ controllers/page.ts
@@ -1,13 +1,26 @@
-import { Request, Response, NextFunction } from "express";
+import { RequestHandler } from "express";
+import { User, Post } from "../models";
 
-export const renderProfile = (req: Request, res: Response) => {
+export const renderProfile: RequestHandler = (req, res) => {
   res.render("profile", { title: "내정보 - 황선구" });
 };
 
-export const renderJoin = (req: Request, res: Response) => {
+export const renderJoin: RequestHandler = (req, res) => {
   res.render("join", { title: "회원가입- 러닝메이트" });
 };
-export const renderMain = (req: Request, res: Response) => {
-  const runItem: Array<string> = [];
-  res.render("main", { title: "Main- 러닝메이트", runItem });
+export const renderComunityPage: RequestHandler = async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ["id", "nick"],
+      },
+      order: [["createdAt", "DESC"]],
+    });
+    res.json(posts);
+    res.end();
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
--- routes/page.ts
+++ routes/page.ts
@@ -1,10 +1,14 @@
 import express from "express";
-import { renderProfile, renderJoin, renderMain } from "../controllers/page";
+import {
+  renderProfile,
+  renderJoin,
+  renderComunityPage,
+} from "../controllers/page";
 import { isLoggedIn, isNotLoggedIn } from "../middlewares";
 
 const router = express.Router();
 router.get("/profile", isLoggedIn, renderProfile);
 router.get("/join", isNotLoggedIn, renderJoin);
-router.get("/", renderMain);
+router.get("/", isLoggedIn, renderComunityPage);
 
 export default router;
