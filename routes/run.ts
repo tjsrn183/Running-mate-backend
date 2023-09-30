851bae94916b988b9c5c276f8a5182207d4fe812
seongu <tjsrn183@naver.com>
2023-09-30 23:02:18 +0900

    rest get메소드를 위해서 수정

--- routes/run.ts
+++ routes/run.ts
@@ -9,6 +9,6 @@ const router = express.Router();
 
 router.post("/register", isLoggedIn, uploadRunItem);
 router.get("/:runItemId", isLoggedIn, getRunItem);
-router.get("/list", isLoggedIn, getRunItemList);
+router.get("/list/:mock", getRunItemList);
 
 export default router;
