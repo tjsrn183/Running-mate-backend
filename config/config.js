2140f9ee134d312b41a153dedf8ab233b59f85bf
seongu <tjsrn183@naver.com>
2023-11-15 23:01:30 +0900

    config파일수정

--- config/config.js
+++ config/config.js
@@ -8,21 +8,21 @@ dotenv_1.default.config();
 const config = {
     development: {
         username: "root",
-        password: process.env.SEQUELIZE_PASSWORD,
+        password: process.env.SEQUELIZE_PASSWORD || "",
         database: "running_mate",
         host: "127.0.0.1",
         dialect: "mysql",
     },
     test: {
         username: "root",
-        password: process.env.SEQUELIZE_PASSWORD,
+        password: process.env.SEQUELIZE_PASSWORD || "",
         database: "database_test",
         host: "127.0.0.1",
         dialect: "mysql",
     },
     production: {
         username: "root",
-        password: process.env.SEQUELIZE_PASSWORD,
+        password: process.env.SEQUELIZE_PASSWORD || "",
         database: "database_production",
         host: "127.0.0.1",
         dialect: "mysql",
--- config/config.ts
+++ config/config.ts
@@ -1,26 +1,50 @@
-import { Dialect } from "sequelize";
-
 import dotenv from "dotenv";
 dotenv.config();
 
-const config = {
+import { Dialect } from "sequelize";
+
+interface Config {
+  development: {
+    username: string;
+    password: string | undefined; // 타입스크립트에서 process.env는 string | undefined로 정의됩니다.
+    database: string;
+    host: string;
+    dialect: Dialect;
+  };
+  test: {
+    username: string;
+    password: string | undefined;
+    database: string;
+    host: string;
+    dialect: Dialect;
+  };
+  production: {
+    username: string;
+    password: string | undefined;
+    database: string;
+    host: string;
+    dialect: Dialect;
+  };
+}
+
+const config: Config = {
   development: {
     username: "root",
-    password: process.env.SEQUELIZE_PASSWORD,
+    password: process.env.SEQUELIZE_PASSWORD || "",
     database: "running_mate",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
     username: "root",
-    password: process.env.SEQUELIZE_PASSWORD,
+    password: process.env.SEQUELIZE_PASSWORD || "",
     database: "database_test",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
     username: "root",
-    password: process.env.SEQUELIZE_PASSWORD,
+    password: process.env.SEQUELIZE_PASSWORD || "",
     database: "database_production",
     host: "127.0.0.1",
     dialect: "mysql",
