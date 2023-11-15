37dd880de98c8efc639ea312257358ac15e47e92
seongu <tjsrn183@naver.com>
2023-11-15 23:40:26 +0900

    config dialect타입수정

--- config/config.ts
+++ config/config.ts
@@ -3,51 +3,29 @@ dotenv.config();
 
 import { Dialect } from "sequelize";
 
-interface Config {
-  development: {
-    username: string;
-    password: string | undefined; // 타입스크립트에서 process.env는 string | undefined로 정의됩니다.
-    database: string;
-    host: string;
-    dialect: Dialect;
-  };
-  test: {
-    username: string;
-    password: string | undefined;
-    database: string;
-    host: string;
-    dialect: Dialect;
-  };
-  production: {
-    username: string;
-    password: string | undefined;
-    database: string;
-    host: string;
-    dialect: Dialect;
-  };
-}
+const dialect: Dialect = "mysql";
 
-const config: Config = {
+const config = {
   development: {
     username: "root",
     password: process.env.SEQUELIZE_PASSWORD || "",
     database: "running_mate",
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: dialect,
   },
   test: {
     username: "root",
     password: process.env.SEQUELIZE_PASSWORD || "",
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: dialect,
   },
   production: {
     username: "root",
     password: process.env.SEQUELIZE_PASSWORD || "",
     database: "database_production",
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: dialect,
   },
 };
 
